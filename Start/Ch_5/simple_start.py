73d11925bf2fb9bcc171b6e01dd4ba3cb9a4f3e9
Radostinna <31807286+Radostinna@users.noreply.github.com>
2023-12-17 19:42:40 +0100

    Update simple_start.py

--- Start/Ch_5/simple_start.py
+++ Start/Ch_5/simple_start.py
@@ -4,3 +4,10 @@
 x = 0
 
 # TODO: Literal patterns are explicit values: integers, strings, Booleans, etc
+match x:
+    case 0:
+      print("Zero")
+    case 1:
+      print("One")
+    case _:
+      print("No match")
